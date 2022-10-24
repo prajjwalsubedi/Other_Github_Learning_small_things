a9e6527e08436ce9d59eda5571f6fdc56e392664
Prajjwal Subedi <66186489+prajjwalsubedi@users.noreply.github.com>
2022-10-24 11:02:00 +1100

    Password manager update final

--- password manager/main.py
+++ password manager/main.py
@@ -1,21 +1,47 @@
 from tkinter import *
+from tkinter import messagebox
+from random import shuffle, randint, choice
+import pyperclip
 
 window = Tk()
 window.title("Password Manager")
 window.config(pady=50, padx=20)
 
-def button_clicked():
-    pass
+
+# Password Generator
+def generate_password():
+
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	
+	password_letters = [choice(letters) for char in range(randint(8, 10))]
+	password_symbols = [choice(symbols) for char in range(randint(2, 4))]
+	password_numbers = [choice(numbers) for char in range(randint(2, 4))]
+	
+	password_list = password_letters + password_numbers + password_symbols
+	
+	shuffle(password_list)
+	
+	password = "".join(password_list)
+	password_input.insert(0, password)
+	pyperclip.copy(password)
+
 
 # Save Password
 def save_details():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
-    with open("data.txt", "a") as data:
-        data.write(f"{website} | {email} | {password}\n")
-        website_input.delete(0, END)
-        password_input.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS !!", message="Please make sure you haven't left any field empty.")
+    else:
+        ask_ok =  messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okey to Save?")
+        if ask_ok:
+            with open("data.txt", "a") as data:
+                data.write(f"{website} | {email} | {password}\n")
+                website_input.delete(0, END)
+                password_input.delete(0, END)
 
 # UI SETUP
 canvas = Canvas(width=200, height=200, highlightthickness=0)
@@ -43,7 +69,7 @@ password.grid(column=1, row=4)
 password_input = Entry(width=20)
 password_input.grid(column=2, row=4)
 
-password_button = Button(text="Generate Password", command=button_clicked)
+password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(column=3, row=4)
 
 add_button = Button(text="Add", command=save_details, width=35)
