521ed27de81108dbd4a3f6605d62c9c4215fb231
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2022-10-25 21:54:23 +1100

    Working on password manager

--- password manager/main.py
+++ password manager/main.py
@@ -10,22 +10,23 @@ window.config(pady=50, padx=20)
 
 # Password Generator
 def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-	
-	password_letters = [choice(letters) for char in range(randint(8, 10))]
-	password_symbols = [choice(symbols) for char in range(randint(2, 4))]
-	password_numbers = [choice(numbers) for char in range(randint(2, 4))]
-	
-	password_list = password_letters + password_numbers + password_symbols
-	
-	shuffle(password_list)
-	
-	password = "".join(password_list)
-	password_input.insert(0, password)
-	pyperclip.copy(password)
+    password_letters = [choice(letters) for char in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for char in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for char in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
 
 # Save Password
@@ -36,12 +37,14 @@ def save_details():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="OOPS !!", message="Please make sure you haven't left any field empty.")
     else:
-        ask_ok =  messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okey to Save?")
+        ask_ok = messagebox.askokcancel(title=website,
+                                        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okey to Save?")
         if ask_ok:
             with open("data.txt", "a") as data:
                 data.write(f"{website} | {email} | {password}\n")
                 website_input.delete(0, END)
                 password_input.delete(0, END)
+
 
 # UI SETUP
 canvas = Canvas(width=200, height=200, highlightthickness=0)
